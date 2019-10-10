40ed6e0daf9d78e0daa3c22a7185c41fc1edabf3
DUCLOS Maxime <mduclos@sodifrance.fr>
2019-10-10 16:52:04 +0200

    Ajout des villes possible

--- getMeteo.js
+++ getMeteo.js
@@ -11,6 +11,37 @@ var tempMoyenne = new Array(END - BEGGIN);
 var pluviometries = new Array(END - BEGGIN);
 var mois = getMoisSaisie();
 const moisDeAnnee = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+const emplacement = ["Angers-Beaucouze",
+	"Belle-Ile-Le-Talut",
+	"Brest-Guipavas",
+	"Brignogan",
+	"Camaret-sur-Mer-Pointe-du-Toulinguet",
+	"Guernesey",
+	"Ile-dYeu-Saint-Sauveur",
+	"Ile-de-Batz",
+	"Ile-de-Brehat",
+	"Ile-de-Groix-Beg-Melen",
+	"Jersey",
+	"La-Roche-sur-Yon-Les-Ajoncs",
+	"Landivisiau",
+	"Lannion-Servel",
+	"Lanveoc-Poulmic",
+	"Laval",
+	"Lorient-Lann-Bihoue",
+	"Nantes-Atlantique",
+	"Ouessant-Stiff",
+	"Pleurtuit-St-Malo",
+	"Ploumanach-Perros",
+	"Pointe-St-Mathieu-Plougonvelin",
+	"Pointe-de-Penmarch",
+	"Pointe-du-Raz",
+	"Quimper-Pluguffan",
+	"Rennes-St-Jacques",
+	"Saint-Nazaire-Montoir",
+	"Saint-Nazaire-Pointe-de-Chemoulin",
+	"Tremuson-St-Brieuc-Armor",
+	"Vannes-Meucon",
+	"Vannes-Sene"];
 fs.unlink(`.\\relever_${moisDeAnnee[parseInt(mois) - 1]}.csv`, (err) => {
 	if (err) {
 		console.error(err);
